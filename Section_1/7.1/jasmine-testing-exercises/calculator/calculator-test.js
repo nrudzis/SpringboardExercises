9be33b902787f81e6f37ee0bf97697371eeb859e
Nick Rudzis <nick.rudzis@gmail.com>
2021-08-24 04:32:57 -0700

    added tests

--- Section_1/7.1/jasmine-testing-exercises/calculator/calculator-test.js
+++ Section_1/7.1/jasmine-testing-exercises/calculator/calculator-test.js
@@ -1,11 +1,22 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  values = {
+    amount: 10000.3,
+    years: 3.4,
+    rate: 5.2
+  }
+  expect(calculateMonthlyPayment(values)).toBeCloseTo(268, 0);
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  values = {
+    amount: 20000,
+    years: 4,
+    rate: 6.7
+  }
+  const monthly = calculateMonthlyPayment(values).split('.');
+  expect(monthly[1].length).toBe(2);
 });
 
 /// etc
