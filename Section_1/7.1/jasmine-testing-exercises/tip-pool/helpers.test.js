bf9d155c805db9665597dee7a27ec6cd5dc20c1c
Nick Rudzis <nick.rudzis@gmail.com>
2021-08-24 08:10:33 -0700

    linked helpers and payments tests to html and wrote helpers tests

--- Section_1/7.1/jasmine-testing-exercises/tip-pool/helpers.test.js
+++ Section_1/7.1/jasmine-testing-exercises/tip-pool/helpers.test.js
@@ -1,3 +1,49 @@
+describe("Helpers test (with setup and tear-down)", function() {
+//setup
+  beforeEach(function() {
+    allPayments = {
+      payment1: {
+        billAmt: 50,
+        tipAmt: 15,
+        tipPercent: calculateTipPercent(50, 15)
+      },
+      payment2: {
+        billAmt: 11,
+        tipAmt: 2,
+        tipPercent: calculateTipPercent(11, 2)
+      }
+    }
+  });
+
 //sumPaymentTotal test
+  it("should calculate payment totals correctly", function() {
+    expect(sumPaymentTotal('billAmt')).toEqual(61);
+    expect(sumPaymentTotal('tipAmt')).toEqual(17);
+    expect(sumPaymentTotal('tipPercent')).toEqual(48);
+  });
+
+
 //calculateTipPercent test
+  it("should calculate tip percent correctly", function() {
+    expect(calculateTipPercent(60, 15)).toEqual(25);
+  });
+
 //appendTd test
+  it("should create new td with innerText and append to tr", function() {
+    let newTr = document.createElement('tr');
+    appendTd(newTr, 'test text');
+    paymentTbody.appendChild(newTr);
+
+    expect(document.querySelector('#paymentTable tbody tr').firstElementChild.nodeName).toEqual('TD');
+    expect(document.querySelector('#paymentTable tbody tr').firstElementChild.innerText).toEqual('test text');
+  });
+
+//tear-down
+  afterEach(function() {
+    allPayments = {};
+    let oldTr = document.querySelector('#paymentTable tbody tr');
+    if (!!oldTr) {
+      paymentTbody.removeChild(oldTr);
+    }
+  });
+});
