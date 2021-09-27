78e620bd0c94dc987f89596cd8b00340759cd91a
Nick Rudzis <nick.rudzis@gmail.com>
2021-09-27 05:49:49 -0700

    changed initial vars to let and const and wrote makeBoard to generate logical board

--- Section_1/11.1/connect-four/connect4.js
+++ Section_1/11.1/connect-four/connect4.js
@@ -5,11 +5,11 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -17,6 +17,12 @@ var board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for(let y = 0; y < HEIGHT; y++) {
+    board[y] = [];
+    for(let x = 0; x < WIDTH; x++) {
+      board[y][x] = null;
+    }
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
