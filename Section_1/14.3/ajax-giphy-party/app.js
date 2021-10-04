efa44770b3af6747284a6542fb7dd8d868d7ffbb
Nick Rudzis <nick.rudzis@gmail.com>
2021-10-04 11:24:53 -0700

    created basic html form and wrote js logic to receive giphy data

--- Section_1/14.3/ajax-giphy-party/app.js
+++ Section_1/14.3/ajax-giphy-party/app.js
@@ -1,1 +1,15 @@
+const $gifSearch = $('#gifSearch');
+
+$('#gifSearchForm').on('submit', async function getGif(e) {
+  e.preventDefault();
+  let res = await axios.get('http://api.giphy.com/v1/gifs/search',
+    {params: { 
+      q: $gifSearch.val(),
+      api_key: 'z9jcxCB03iip1t6xy609JyogrPQDhIzA'
+    }
+  });
+  console.log(res.data.data);
+  $gifSearch.val('');
+});
+
 console.log("Let's get this party started!");
