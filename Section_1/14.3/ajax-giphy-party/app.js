1b889db8b99ce145fe3de1723732d64e51b4fcf9
Nick Rudzis <nick.rudzis@gmail.com>
2021-10-04 12:25:56 -0700

    added logic to append gifs to page

--- Section_1/14.3/ajax-giphy-party/app.js
+++ Section_1/14.3/ajax-giphy-party/app.js
@@ -1,5 +1,7 @@
 const $gifSearch = $('#gifSearch');
 
+$('<ul>').attr('id', 'gifList').insertAfter('#gifSearchForm');
+
 $('#gifSearchForm').on('submit', async function getGif(e) {
   e.preventDefault();
   let res = await axios.get('http://api.giphy.com/v1/gifs/search',
@@ -8,7 +10,9 @@ $('#gifSearchForm').on('submit', async function getGif(e) {
       api_key: 'z9jcxCB03iip1t6xy609JyogrPQDhIzA'
     }
   });
-  console.log(res.data.data);
+  randIndex = Math.floor(Math.random() * (res.data.data.length + 1))
+  imgSrc = res.data.data[randIndex].images.original.url;
+  $('#gifList').append('<li>').append(`<img src=${imgSrc}>`);
   $gifSearch.val('');
 });
 
