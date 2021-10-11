457493d2998f0075e29414a50ff291aeea93aa84
Nick Rudzis <nick.rudzis@gmail.com>
2021-10-10 17:33:45 -0700

    added logic for searchShows

--- Section_1/14.3/apis-tvmaze/tvmaze.js
+++ Section_1/14.3/apis-tvmaze/tvmaze.js
@@ -19,19 +19,18 @@
  */
 async function searchShows(query) {
   // TODO: Make an ajax request to the searchShows api.  Remove
-  // hard coded data.
-
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  // hard coded data. - DONE
+  let res = await axios.get(`https://api.tvmaze.com/search/shows?q=${query}`);
+  return res.data.reduce((shows, nextShow) => {
+    shows.push({
+      id: nextShow.show.id,
+      name: nextShow.show.name,
+      summary: nextShow.show.summary,
+      image: nextShow.show.image ? nextShow.show.image.original : 'https://tinyurl.com/tv-missing'
+    });
+    return shows;
+  }, []);
 }
-
-
 
 /** Populate shows list:
  *     - given list of shows, add shows to DOM
@@ -81,10 +80,10 @@ $("#search-form").on("submit", async function handleSearch (evt) {
  *      { id, name, season, number }
  */
 
-async function getEpisodes(id) {
+//async function getEpisodes(id) {
   // TODO: get episodes from tvmaze
   //       you can get this by making GET request to
   //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
 
   // TODO: return array-of-episode-info, as described in docstring above
-}
+//}
