93bb7cda614f4750f783b0354f7b43353bb0b939
Nick Rudzis <nick.rudzis@gmail.com>
2021-10-29 01:52:13 -0700

    added logic to prevent titles with less than 2 characters and ensure ratings between 0 and 10

--- Section_1/13.1/movie-app/script.js
+++ Section_1/13.1/movie-app/script.js
@@ -4,6 +4,16 @@ $('form').on('submit', (e) => {
   e.preventDefault();
   let title = $('#title').val();
   let rating = $('#rating').val();
+  if(title.length < 2) {
+    alert('Title must be at least 2 characters!');
+    resetForm();
+    return;
+  }
+  if(rating < 0 || rating > 10) {
+    alert('Rating must be between 0 and 10!');
+    resetForm();
+    return;
+  }
   movies[title] = rating;
   console.log(movies);
   $('#movie-area').append(
@@ -12,8 +22,7 @@ $('form').on('submit', (e) => {
       <button>Remove</button>
     </div>`
   );
-  $('#title').val('');
-  $('#rating').val('');
+  resetForm();
 });
 
 $('#movie-area').on('click', 'button', (e) => {
@@ -21,3 +30,8 @@ $('#movie-area').on('click', 'button', (e) => {
   console.log(movies);
   $(e.currentTarget).parent().remove();
 });
+
+const resetForm = () => {
+  $('#title').val('');
+  $('#rating').val('');
+}
