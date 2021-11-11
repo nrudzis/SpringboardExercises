01887876d694714c49ad954e759227b988fc4a55
Nick Rudzis <nick.rudzis@gmail.com>
2021-11-10 16:32:03 -0800

    wrote convert

--- Section_2/18.1/python-syntax/convert.py
+++ Section_2/18.1/python-syntax/convert.py
@@ -1,7 +1,7 @@
 def convert_temp(unit_in, unit_out, temp):
     """Convert farenheit <-> celsius and return results.
 
-    - unit_in: either "f" or "c" 
+    - unit_in: either "f" or "c"
     - unit_out: either "f" or "c"
     - temp: temperature (in f or c, depending on unit_in)
 
@@ -15,7 +15,16 @@ def convert_temp(unit_in, unit_out, temp):
       convert_temp("f", "c", 212) => 100.0
     """
 
-    # YOUR CODE HERE
+    if unit_in != "c" and unit_in != "f":
+        return f"Invalid unit {unit_in}"
+    elif unit_out != "c" and unit_out != "f":
+        return f"Invalid unit {unit_out}"
+    elif unit_in == unit_out:
+        return temp
+    elif unit_in == "c":
+        return temp * 9 / 5 + 32
+    else:
+        return (temp - 32) * 5 / 9
 
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
@@ -23,4 +32,3 @@ print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
 print("z", "f", 32, convert_temp("z", "f", 32), "should be Invalid unit z")
 print("c", "z", 32, convert_temp("c", "z", 32), "should be Invalid unit z")
 print("f", "f", 75.5, convert_temp("f", "f", 75.5), "should be 75.5")
-
