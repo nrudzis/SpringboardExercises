e355e73cc6044bd77a49bf2db3648eaf59b48df0
Nick Rudzis <nick.rudzis@gmail.com>
2021-11-10 16:09:39 -0800

    wrote count_up

--- Section_2/18.1/python-syntax/count_up.py
+++ Section_2/18.1/python-syntax/count_up.py
@@ -12,7 +12,9 @@ def count_up(start, stop):
         7
     """
 
-    # YOUR CODE HERE
+    while start <= stop:
+        print(start)
+        start += 1
 
 
-count_up(5, 7)        
+count_up(5, 7)
