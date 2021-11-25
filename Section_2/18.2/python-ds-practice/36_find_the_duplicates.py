dae36ea89e7e6c840b13e460c5d006eeac5e0bb9
Nick Rudzis <nick.rudzis@gmail.com>
2021-11-25 08:00:59 -0800

    wrote logic for find_the_duplicates

--- Section_2/18.2/python-ds-practice/36_find_the_duplicates.py
+++ Section_2/18.2/python-ds-practice/36_find_the_duplicates.py
@@ -13,3 +13,7 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+    for num in nums:
+        nums.remove(num)
+        if num in nums:
+            return num
