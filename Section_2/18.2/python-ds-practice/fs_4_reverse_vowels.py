5bea9aacee3210de591216ce1d24040dee3912c6
Nick Rudzis <nick.rudzis@gmail.com>
2021-11-25 08:14:47 -0800

    wrote logic for reverse_vowels

--- Section_2/18.2/python-ds-practice/fs_4_reverse_vowels.py
+++ Section_2/18.2/python-ds-practice/fs_4_reverse_vowels.py
@@ -19,3 +19,10 @@ def reverse_vowels(s):
     reverse_vowels("why try, shy fly?")
     'why try, shy fly?''
     """
+    s_vowels = [c for c in s if c.lower() in 'aeiou']
+    letters = list(s)
+    for i in range(len(letters)):
+        if letters[i].lower() in 'aeiou':
+            sub = s_vowels.pop()
+            letters[i] = sub
+    return ''.join(letters)
