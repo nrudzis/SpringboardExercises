04741326e62183f66d23e5157d663dd2da93083d
Nick Rudzis <nick.rudzis@gmail.com>
2021-11-25 08:05:00 -0800

    wrote logic for min_max_key_in_dictionary

--- Section_2/18.2/python-ds-practice/38_min_max_key_in_dictionary.py
+++ Section_2/18.2/python-ds-practice/38_min_max_key_in_dictionary.py
@@ -9,3 +9,4 @@ def min_max_keys(d):
         >>> min_max_keys({"apple": "red", "cherry": "red", "berry": "blue"})
         ('apple', 'cherry')
     """
+    return (sorted(d.keys())[0], sorted(d.keys())[-1])
