449a6fd6bd58f64a9ec3f8e058fa2c82f242095d
Nick Rudzis <nick.rudzis@gmail.com>
2021-11-25 07:45:47 -0800

    wrote logic for two_list_dictionary

--- Section_2/18.2/python-ds-practice/32_two_list_dictionary.py
+++ Section_2/18.2/python-ds-practice/32_two_list_dictionary.py
@@ -15,3 +15,6 @@ def two_list_dictionary(keys, values):
         >>> two_list_dictionary(['a', 'b', 'c'], [1, 2, 3, 4])
         {'a': 1, 'b': 2, 'c': 3}
    """
+    while len(keys) > len(values):
+        values.append(None)
+    return {keys[i]: values[i] for i in range(len(keys))}
