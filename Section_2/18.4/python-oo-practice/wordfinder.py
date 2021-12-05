29dbf094dacf34d9d68fa02d5ac8c8a8fc92b116
Nick Rudzis <nick.rudzis@gmail.com>
2021-12-05 09:14:28 -0800

    wrote logic for WordFinder

--- Section_2/18.4/python-oo-practice/wordfinder.py
+++ Section_2/18.4/python-oo-practice/wordfinder.py
@@ -1,5 +1,16 @@
 """Word Finder: finds random words from a dictionary."""
 
+import random
 
 class WordFinder:
-    ...
+    def __init__(self, path):
+        file = open(path, "r")
+        self.words = self.parse(file)
+        print(f"{len(self.words)} words read")
+        file.close()
+
+    def parse(self, file):
+        return [word.rstrip("\n") for word in file]
+
+    def random(self):
+        return random.choice(self.words)
