40956a6d7ac41aed1354f07ef665f5f54fd7dd0b
Nick Rudzis <nick.rudzis@gmail.com>
2021-12-05 09:29:48 -0800

    wrote docstrings for SpecialWordFinder

--- Section_2/18.4/python-oo-practice/wordfinder.py
+++ Section_2/18.4/python-oo-practice/wordfinder.py
@@ -20,9 +20,12 @@ class WordFinder:
         return random.choice(self.words)
 
 class SpecialWordFinder(WordFinder):
+    """Find words in a file, and exclude comments and blank lines."""
     def __init__(self, path):
+        """Print number of words found."""
         super().__init__(path)
 
     def parse(self, file):
+        """Remove new line character from words, and remove blank lines and words with the hash symbol."""
         words = super().parse(file)
         return [word for word in words if word != "" and "#" not in word]
