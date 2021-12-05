98f68ec7a817cfa7e5bb5ad106473af29fd82aa1
Nick Rudzis <nick.rudzis@gmail.com>
2021-12-05 09:20:56 -0800

    wrote docstrings for WordFinder

--- Section_2/18.4/python-oo-practice/wordfinder.py
+++ Section_2/18.4/python-oo-practice/wordfinder.py
@@ -3,14 +3,18 @@
 import random
 
 class WordFinder:
+    """Find words in a file."""
     def __init__(self, path):
+        """Print number of words found."""
         file = open(path, "r")
         self.words = self.parse(file)
         print(f"{len(self.words)} words read")
         file.close()
 
     def parse(self, file):
+        """Remove new line character from words."""
         return [word.rstrip("\n") for word in file]
 
     def random(self):
+        """Display random word."""
         return random.choice(self.words)
