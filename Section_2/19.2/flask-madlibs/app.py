42d7eabb1971e6bfb002f0cb9d2ffa63676ac6e1
Nick Rudzis <nick.rudzis@gmail.com>
2022-01-17 13:50:08 -0800

    improved variable names and changed story_page route to utilize post requests

--- Section_2/19.2/flask-madlibs/app.py
+++ Section_2/19.2/flask-madlibs/app.py
@@ -11,23 +11,23 @@ debug = DebugToolbarExtension(app)
 @app.route('/')
 def select_story():
     """Display selection menu of story templates"""
-    options = stories.stories.keys()
-    return render_template("select.html", stories=options)
+    story_names = stories.stories.keys()
+    return render_template('select.html', story_names=story_names)
 
 
 @app.route('/prompt')
 def prompt_page():
     """Prompt for needed words"""
-    selected_story = request.args["selected_story"]
-    p, s = stories.stories[selected_story]
-    return render_template("prompt.html", selected_story=selected_story, prompts=p)
+    selected_story_name = request.args['selected_story']
+    p, s = stories.stories[selected_story_name]
+    return render_template('prompt.html', selected_story_name=selected_story_name, prompts=p)
 
 
-@app.route('/story')
-def story_page():
+@app.route('/story/<selected_story_name>', methods=['POST'])
+def story_page(selected_story_name):
     """Display generated story"""
-    p, s = stories.stories[request.args.get("selected_story")]
+    p, s = stories.stories[selected_story_name]
     story = stories.Story(p, s)
-    ans = {prompt: request.args[prompt] for prompt in story.prompts}
+    ans = {prompt: request.form[prompt] for prompt in story.prompts}
     your_story = story.generate(ans)
-    return render_template("story.html", your_story=your_story)
+    return render_template('story.html', your_story=your_story)
