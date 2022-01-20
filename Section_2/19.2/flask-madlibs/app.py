7c314a872e624d75350e70dfe37d24af6b85ad34
Nick Rudzis <nick.rudzis@gmail.com>
2022-01-20 02:48:31 -0800

    changed story_page route to utilize get method

--- Section_2/19.2/flask-madlibs/app.py
+++ Section_2/19.2/flask-madlibs/app.py
@@ -23,11 +23,11 @@ def prompt_page():
     return render_template('prompt.html', selected_story_name=selected_story_name, prompts=p)
 
 
-@app.route('/story/<selected_story_name>', methods=['POST'])
+@app.route('/story/<selected_story_name>')
 def story_page(selected_story_name):
     """Display generated story"""
     p, s = stories.stories[selected_story_name]
     story = stories.Story(p, s)
-    ans = {prompt: request.form[prompt] for prompt in story.prompts}
+    ans = {prompt: request.args[prompt] for prompt in story.prompts}
     your_story = story.generate(ans)
     return render_template('story.html', your_story=your_story)
