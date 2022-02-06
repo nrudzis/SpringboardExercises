a5839d5597f6f8bfd4b14ff57a2f1308a4d85b6e
Nick Rudzis <nick.rudzis@gmail.com>
2022-02-06 07:00:11 -0800

    added route to handle survey answers and thank you route and template for completed survey

--- Section_2/19.3/flask-survey/app.py
+++ Section_2/19.3/flask-survey/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 import surveys
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 responses = []
@@ -26,3 +27,18 @@ def question_page(q_number):
     question = survey_question.question
     choices = survey_question.choices
     return render_template("questions.html", survey_title=satisfaction_survey_title, q_number=q_number, question=question, choices=choices)
+
+
+@app.route('/answer', methods=["POST"])
+def handle_answers():
+    """Append answers to responses list and redirect to the next question"""
+    answer = request.form["answer"]
+    responses.append(answer)
+    q_number = request.form["q_number"]
+    next_q_number = str(int(q_number) + 1)
+    return redirect(url_for('question_page', q_number=next_q_number) if int(next_q_number) < len(satisfaction_survey.questions) else '/thank-you')
+
+
+@app.route('/thank-you')
+def say_thanks():
+    return render_template("thanks.html", survey_title=satisfaction_survey_title)
