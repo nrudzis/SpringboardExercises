f9c14b5807859153c85955961b02cca0342e80db
Nick Rudzis <nick.rudzis@gmail.com>
2022-02-06 05:29:19 -0800

    added route and template for questions page

--- Section_2/19.3/flask-survey/app.py
+++ Section_2/19.3/flask-survey/app.py
@@ -8,12 +8,21 @@ app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 responses = []
+satisfaction_survey = surveys.surveys["satisfaction"]
+satisfaction_survey_title = satisfaction_survey.title
 
 
 @app.route('/')
 def start_page():
     """Render start page."""
-    survey = surveys.surveys["satisfaction"]
-    survey_title = survey.title
-    survey_instructions = survey.instructions
-    return render_template("home.html", survey_title=survey_title, survey_instructions=survey_instructions)
+    survey_instructions = satisfaction_survey.instructions
+    return render_template("home.html", survey_title=satisfaction_survey_title, survey_instructions=survey_instructions)
+
+
+@app.route('/questions/<q_number>')
+def question_page(q_number):
+    """Display survey question."""
+    survey_question = satisfaction_survey.questions[int(q_number)]
+    question = survey_question.question
+    choices = survey_question.choices
+    return render_template("questions.html", survey_title=satisfaction_survey_title, q_number=q_number, question=question, choices=choices)
