4f7b09ab1f882ea8f665a434483e6ff6978362de
Nick Rudzis <nick.rudzis@gmail.com>
2022-02-06 03:44:35 -0800

    created empty list to hold user responses and added route and templates for start page

--- Section_2/19.3/flask-survey/app.py
+++ Section_2/19.3/flask-survey/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+import surveys
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.route('/')
+def start_page():
+    """Render start page."""
+    survey = surveys.surveys["satisfaction"]
+    survey_title = survey.title
+    survey_instructions = survey.instructions
+    return render_template("home.html", survey_title=survey_title, survey_instructions=survey_instructions)
