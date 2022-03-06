f6a4faaad59e922d4b7e8c1536beda143da2b051
Nick Rudzis <nick.rudzis@gmail.com>
2022-03-06 06:58:53 -0800

    installed flask and flask debug toolbar, created requirements file, created html file in templates directory to display boggle board, added first route and view function

--- Section_2/19.5/flask-boggle/app.py
+++ Section_2/19.5/flask-boggle/app.py
@@ -1,3 +1,19 @@
+from flask import Flask, render_template
 from boggle import Boggle
+from flask_debugtoolbar import DebugToolbarExtensions
+
+app = Flask(__name___)
+
+app.config['SECRET_KEY'] = "secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+
+@app.route('/')
+def boggle_board_page():
+    """Render game board."""
+    session["board"] = boggle_game.make_board()
+    board = session["board"]
+    return render_template("boggle.html", board=board)
