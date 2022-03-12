59e1bf7474c05f7fa3a7b5708fa6b9b3870c28f5
Nick Rudzis <nick.rudzis@gmail.com>
2022-03-11 16:50:22 -0800

    added score keeping, changed word result message to display current message only

--- Section_2/19.5/flask-boggle/static/checkWord.js
+++ Section_2/19.5/flask-boggle/static/checkWord.js
@@ -1,11 +1,23 @@
+function keepScore(word, result) {
+  if(result === "ok") {
+    let score = parseInt($('#score').text());
+    score = score + word.length;
+    $('#score').text(score.toString());
+  }
+}
+
 async function checkWord(e) {
-    e.preventDefault();
-    const word = $('#word').val();
-    const response = await axios.get('/check-word', { params: { word } });
-    const result = response.data.result;
-    const $message = $(`<p>${ word } is ${ result.replace(/-/g, " ") }</p>`);
-    $('#word-form').after($message);
-    $('#word').val('');
+  e.preventDefault();
+  const word = $('#word').val();
+  const response = await axios.get('/check-word', { params: { word } });
+  const result = response.data.result;
+  const $message = $(`<p>${ word } is ${ result.replace(/-/g, " ") }</p>`);
+  if( $('#word-form').next('p') ) {
+    $('#word-form').next('p').remove();
+  }
+  $('#word-form').after($message);
+  $('#word').val('');
+  keepScore(word, result);
 }
 
 $('#word-form').on('submit', checkWord);
