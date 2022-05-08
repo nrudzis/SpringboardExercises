0589e127b9e7c1593c588865b47899a2c7697e8a
Nick Rudzis <nick.rudzis@gmail.com>
2022-05-08 07:42:32 -0700

    improved schemas and modified seed files

--- Section_2/22.4/sql-ddl-design/air_traffic.sql
+++ Section_2/22.4/sql-ddl-design/air_traffic.sql
@@ -1,37 +1,104 @@
--- from the terminal run:
--- psql < air_traffic.sql
-
 DROP DATABASE IF EXISTS air_traffic;
 
 CREATE DATABASE air_traffic;
 
 \c air_traffic
 
-CREATE TABLE tickets
-(
+CREATE TABLE passenger (
   id SERIAL PRIMARY KEY,
   first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL,
-  seat TEXT NOT NULL,
+  last_name TEXT NOT NULL
+);
+
+CREATE TABLE airline (
+  id SERIAL PRIMARY KEY,
+  airline_name TEXT NOT NULL
+);
+
+CREATE TABLE flight (
+  id SERIAL PRIMARY KEY,
   departure TIMESTAMP NOT NULL,
   arrival TIMESTAMP NOT NULL,
-  airline TEXT NOT NULL,
   from_city TEXT NOT NULL,
   from_country TEXT NOT NULL,
   to_city TEXT NOT NULL,
-  to_country TEXT NOT NULL
+  to_country TEXT NOT NULL,
+  airline_id INTEGER REFERENCES airline ON DELETE SET NULL
 );
 
-INSERT INTO tickets
-  (first_name, last_name, seat, departure, arrival, airline, from_city, from_country, to_city, to_country)
+CREATE TABLE seat (
+  id SERIAL PRIMARY KEY,
+  seat_label TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE ticket (
+  id SERIAL PRIMARY KEY,
+  passenger_id INTEGER NOT NULL REFERENCES passenger ON DELETE CASCADE,
+  flight_id INTEGER NOT NULL REFERENCES flight ON DELETE CASCADE,
+  seat_id INTEGER REFERENCES seat ON DELETE SET NULL
+);
+
+INSERT INTO passenger
+  (first_name, last_name)
 VALUES
-  ('Jennifer', 'Finch', '33B', '2018-04-08 09:00:00', '2018-04-08 12:00:00', 'United', 'Washington DC', 'United States', 'Seattle', 'United States'),
-  ('Thadeus', 'Gathercoal', '8A', '2018-12-19 12:45:00', '2018-12-19 16:15:00', 'British Airways', 'Tokyo', 'Japan', 'London', 'United Kingdom'),
-  ('Sonja', 'Pauley', '12F', '2018-01-02 07:00:00', '2018-01-02 08:03:00', 'Delta', 'Los Angeles', 'United States', 'Las Vegas', 'United States'),
-  ('Jennifer', 'Finch', '20A', '2018-04-15 16:50:00', '2018-04-15 21:00:00', 'Delta', 'Seattle', 'United States', 'Mexico City', 'Mexico'),
-  ('Waneta', 'Skeleton', '23D', '2018-08-01 18:30:00', '2018-08-01 21:50:00', 'TUI Fly Belgium', 'Paris', 'France', 'Casablanca', 'Morocco'),
-  ('Thadeus', 'Gathercoal', '18C', '2018-10-31 01:15:00', '2018-10-31 12:55:00', 'Air China', 'Dubai', 'UAE', 'Beijing', 'China'),
-  ('Berkie', 'Wycliff', '9E', '2019-02-06 06:00:00', '2019-02-06 07:47:00', 'United', 'New York', 'United States', 'Charlotte', 'United States'),
-  ('Alvin', 'Leathes', '1A', '2018-12-22 14:42:00', '2018-12-22 15:56:00', 'American Airlines', 'Cedar Rapids', 'United States', 'Chicago', 'United States'),
-  ('Berkie', 'Wycliff', '32B', '2019-02-06 16:28:00', '2019-02-06 19:18:00', 'American Airlines', 'Charlotte', 'United States', 'New Orleans', 'United States'),
-  ('Cory', 'Squibbes', '10D', '2019-01-20 19:30:00', '2019-01-20 22:45:00', 'Avianca Brasil', 'Sao Paolo', 'Brazil', 'Santiago', 'Chile');
+  ('Jennifer', 'Finch'),
+  ('Thadeus', 'Gathercoal'),
+  ('Sonja', 'Pauley'),
+  ('Waneta', 'Skeleton'),
+  ('Berkie', 'Wycliff'),
+  ('Alvin', 'Leathes'),
+  ('Cory', 'Squibbes');
+
+INSERT INTO airline
+  (airline_name)
+VALUES
+  ('United'),
+  ('British Airways'),
+  ('Delta'),
+  ('TUI Fly Belgium'),
+  ('Air China'),
+  ('American Airlines'),
+  ('Avianca Brazil');
+
+INSERT INTO flight
+  (departure, arrival, from_city, from_country, to_city, to_country, airline_id)
+VALUES
+  ('2018-04-08 09:00:00', '2018-04-08 12:00:00', 'Washington DC', 'United States', 'Seattle', 'United States', 1),
+  ('2018-12-19 12:45:00', '2018-12-19 16:15:00', 'Tokyo', 'Japan', 'London', 'United Kingdom', 2),
+  ('2018-01-02 07:00:00', '2018-01-02 08:03:00', 'Los Angeles', 'United States', 'Las Vegas', 'United States', 3),
+  ('2018-04-15 16:50:00', '2018-04-15 21:00:00', 'Seattle', 'United States', 'Mexico City', 'Mexico', 3),
+  ('2018-08-01 18:30:00', '2018-08-01 21:50:00', 'Paris', 'France', 'Casablanca', 'Morocco', 4),
+  ('2018-10-31 01:15:00', '2018-10-31 12:55:00', 'Dubai', 'UAE', 'Beijing', 'China', 5),
+  ('2019-02-06 06:00:00', '2019-02-06 07:47:00', 'New York', 'United States', 'Charlotte', 'United States', 1),
+  ('2018-12-22 14:42:00', '2018-12-22 15:56:00', 'Cedar Rapids', 'United States', 'Chicago', 'United States', 6),
+  ('2019-02-06 16:28:00', '2019-02-06 19:18:00', 'Charlotte', 'United States', 'New Orleans', 'United States', 6),
+  ('2019-01-20 19:30:00', '2019-01-20 22:45:00', 'Sao Paolo', 'Brazil', 'Santiago', 'Chile', 7);
+
+INSERT INTO seat
+  (seat_label)
+VALUES
+  -- probably would generate this by looping over and combining letters and numbers, but just using sample stand-ins for this exercise
+  ('33B'),
+  ('8A'),
+  ('12F'),
+  ('20A'),
+  ('23D'),
+  ('18C'),
+  ('9E'),
+  ('1A'),
+  ('32B'),
+  ('10D');
+
+INSERT INTO ticket
+  (passenger_id, flight_id, seat_id)
+VALUES
+  (1, 1, 1),
+  (2, 2, 2),
+  (3, 3, 3),
+  (1, 4, 4),
+  (4, 5, 5),
+  (2, 6, 6),
+  (5, 7, 7),
+  (6, 8, 8),
+  (5, 9, 9),
+  (7, 10, 10);
--- Section_2/22.4/sql-ddl-design/outer_space.sql
+++ Section_2/22.4/sql-ddl-design/outer_space.sql
@@ -1,28 +1,72 @@
--- from the terminal run:
--- psql < outer_space.sql
-
 DROP DATABASE IF EXISTS outer_space;
 
 CREATE DATABASE outer_space;
 
 \c outer_space
 
-CREATE TABLE planets
-(
+CREATE TABLE galaxy (
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  galaxy_name TEXT NOT NULL
 );
 
-INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+CREATE TABLE star (
+  id SERIAL PRIMARY KEY,
+  star_name TEXT NOT NULL,
+  galaxy_id INTEGER NOT NULL REFERENCES galaxy ON DELETE CASCADE
+);
+
+CREATE TABLE planet (
+  id SERIAL PRIMARY KEY,
+  planet_name TEXT NOT NULL,
+  orbital_period_in_years FLOAT NOT NULL,
+  orbits_around INTEGER NOT NULL REFERENCES star ON DELETE CASCADE
+);
+
+CREATE TABLE moon (
+  id SERIAL PRIMARY KEY,
+  moon_name TEXT NOT NULL,
+  orbits_around INTEGER NOT NULL REFERENCES planet ON DELETE CASCADE
+);
+
+INSERT INTO galaxy
+  (galaxy_name)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  ('Milky Way');
+
+INSERT INTO star
+  (star_name, galaxy_id)
+VALUES
+  ('The Sun', 1),
+  ('Proxima Centauri', 1),
+  ('Gliese 876', 1);
+
+INSERT INTO planet
+  (planet_name, orbital_period_in_years, orbits_around)
+VALUES
+  ('Earth', 1.00, 1),
+  ('Mars', 1.88, 1),
+  ('Venus', 0.62, 1),
+  ('Neptune', 164.8, 1),
+  ('Proxima Centauri b', 0.03, 2),
+  ('Gliese 876 b', 0.23, 3);
+
+INSERT INTO moon
+  (moon_name, orbits_around)
+VALUES
+  ('The Moon', 1),
+  ('Phobos', 2),
+  ('Deimos', 2),
+  ('Naiad', 4),
+  ('Thalassa', 4),
+  ('Despina', 4),
+  ('Galatea', 4),
+  ('Larissa', 4),
+  ('S/2004 N 1', 4),
+  ('Proteus', 4),
+  ('Triton', 4),
+  ('Nereid', 4),
+  ('Halimede', 4),
+  ('Sao', 4),
+  ('Laomedeia', 4),
+  ('Psamathe', 4),
+  ('Neso', 4);
