ea34e934cec1410acc75e49ede25205d431b1cf8
Nick Rudzis <nick.rudzis@gmail.com>
2022-05-09 12:27:52 -0700

    installed tools, created user model and flask app

---
+++ Section_2/23.1/flask-blogly/app.py
@@ -0,0 +1,16 @@
+"""Blogly application."""
+
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
---
+++ Section_2/23.1/flask-blogly/models.py
@@ -0,0 +1,36 @@
+"""Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = 'user'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    first_name = db.Column(
+        db.String(20),
+        nullable=False
+    )
+
+    last_name = db.Column(
+        db.String(20),
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.String(100),
+        nullable=False,
+        default='https://cdn-icons-png.flaticon.com/512/1946/1946429.png'
+    )
