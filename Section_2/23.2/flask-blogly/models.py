49118a1cb9b2a3325326af387c389a0341e95b30
Nick Rudzis <nick.rudzis@gmail.com>
2022-05-16 10:29:09 -0700

    created post model

--- Section_2/23.2/flask-blogly/models.py
+++ Section_2/23.2/flask-blogly/models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
 
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -34,3 +35,36 @@ class User(db.Model):
         nullable=False,
         default='/static/user-solid.svg'
     )
+
+class Post(db.Model):
+    """Post model."""
+
+    __tablename__ = 'posts'
+
+    post_id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    title = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    content = db.Column(
+        db.String(500),
+        nullable=False
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.now
+    )
+
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('users.user_id')
+    )
+
+    user = db.relationship('User', backref='posts')
