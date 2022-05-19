9789f986935be41e4eedebe4a31bfc4261f579c7
Nick Rudzis <nick.rudzis@gmail.com>
2022-05-19 10:41:41 -0700

    added form validators

--- Section_2/24.1/flask-adopt/forms.py
+++ Section_2/24.1/flask-adopt/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField
+from wtforms.validators import InputRequired, Optional, URL, NumberRange, AnyOf
 
 class NewPetForm(FlaskForm):
-    name = StringField('Pet Name')
-    species = StringField('Species')
-    photo_url = StringField('Photo URL')
-    age = FloatField('Age')
+    name = StringField('Pet Name', validators=[InputRequired()])
+    species = StringField('Species', validators=[AnyOf(('cat', 'dog', 'porcupine'), message='Must be "cat", "dog", or "porcupine"')])
+    photo_url = StringField('Photo URL', validators=[Optional(), URL(message='Must be a valid URL')])
+    age = FloatField('Age', validators=[NumberRange(min=0, max=30, message='Must be between 0 and 30')])
     notes = StringField('Notes')
