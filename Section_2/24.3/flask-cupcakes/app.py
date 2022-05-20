6bcf618f302ba2f4018191751174771685c9a26d
Nick Rudzis <nick.rudzis@gmail.com>
2022-05-20 05:35:56 -0700

    created initial files, installed dependencies

---
+++ Section_2/24.3/flask-cupcakes/app.py
@@ -0,0 +1,16 @@
+"""Cupcake application."""
+
+from flask import Flask, request, jsonify, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
---
+++ Section_2/24.3/flask-cupcakes/models.py
@@ -0,0 +1,37 @@
+"""Models for Cupcakes."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Cupcake model."""
+
+    __tablename__ = 'cupcakes'
+
+    cupcake_id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    flavor = db.Column(
+        db.Text,
+        nullable=False
+    )
+    size = db.Column(
+        db.Text,
+        nullable=False
+    )
+    rating = db.Column(
+        db.Float,
+        nullable=False
+    )
+    image = db.Column(
+        db.Text,
+        default='https://tinyurl.com/demo-cupcake',
+        nullable=False
+    )
