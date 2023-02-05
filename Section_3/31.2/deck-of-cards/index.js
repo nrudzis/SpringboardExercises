2b14a84def54dc30200c4b4612fe8418516e96ab
Nick Rudzis <nick.rudzis@gmail.com>
2023-02-05 10:48:10 -0800

    refactor js to draw all card in one request

--- Section_3/31.2/deck-of-cards/index.js
+++ Section_3/31.2/deck-of-cards/index.js
@@ -5,18 +5,18 @@ const cardImgs = [];
 axios
   .get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
   .then(d => {
-    for (let i=0; i<52; i++) {
-      axios
-        .get(`https://deckofcardsapi.com/api/deck/${ d.data.deck_id }/draw/?count=1`)
-        .then(c => {
-          const cardSVG = c.data.cards[0]['images']['svg'];
+    axios
+      .get(`https://deckofcardsapi.com/api/deck/${ d.data.deck_id }/draw/?count=52`)
+      .then(c => {
+        for (let i=0; i<c.data.cards.length; i++) {
+          const cardSVG = c.data.cards[i]['images']['svg'];
           const img = document.createElement('img');
           img.src = cardSVG
           img.classList.add('overlay');
           cardImgs.push(img);
-        })
-        .catch(err => console.log('ERROR!', err));
-    }
+        }
+      })
+      .catch(err => console.log('ERROR!', err));
   })
   .catch(err => console.log('ERROR!', err));
 
