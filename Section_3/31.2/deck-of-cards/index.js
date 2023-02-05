e7e16c429d5a91e5a95397842877db7dc9a4f1bc
Nick Rudzis <nick.rudzis@gmail.com>
2023-02-05 09:59:11 -0800

    refactor js to speed up card loading on page

--- Section_3/31.2/deck-of-cards/index.js
+++ Section_3/31.2/deck-of-cards/index.js
@@ -1,26 +1,32 @@
 const cardBtn = document.getElementById('draw-card-btn');
 const cardSection = document.getElementById('card-section');
-
-function drawCard(deckId) {
-  axios
-    .get(`https://deckofcardsapi.com/api/deck/${ deckId }/draw/?count=1`)
-    .then(c => {
-      if (c.data.remaining > 0) {
-        const cardImg = c.data.cards[0]['images']['svg'];
-        const img = document.createElement('img');
-        img.src = cardImg;
-        img.classList.add('overlay');
-        cardSection.appendChild(img);
-      } else {
-        cardBtn.disabled = true;
-      }
-    })
-    .catch(err => console.log('ERROR!', err));
-}
+const cardImgs = [];
 
 axios
   .get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
   .then(d => {
-    cardBtn.addEventListener('click', e => drawCard(d.data.deck_id));
+    for (let i=0; i<52; i++) {
+      axios
+        .get(`https://deckofcardsapi.com/api/deck/${ d.data.deck_id }/draw/?count=1`)
+        .then(c => {
+          const cardSVG = c.data.cards[0]['images']['svg'];
+          const img = document.createElement('img');
+          img.src = cardSVG
+          img.classList.add('overlay');
+          cardImgs.push(img);
+        })
+        .catch(err => console.log('ERROR!', err));
+    }
   })
   .catch(err => console.log('ERROR!', err));
+
+const drawCard = () => {
+  const cardImg = cardImgs[0];
+  cardSection.appendChild(cardImg);
+  cardImgs.shift();
+  if (cardImgs.length == 0) {
+    cardBtn.disabled = true;
+  }
+}
+
+cardBtn.addEventListener('click', e => drawCard());
