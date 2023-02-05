5b0945829a37aa35920073fc57ecaee6e9c0dea7
Nick Rudzis <nick.rudzis@gmail.com>
2023-02-05 07:39:35 -0800

    add last item of part 1

--- Section_3/31.2/number-facts/index.js
+++ Section_3/31.2/number-facts/index.js
@@ -1,22 +1,36 @@
 const numFactSection = document.querySelector('#num-fact-section');
-let numFact = axios.get('http://numbersapi.com/42?json');
 
-numFact
-  .then(res => {
-    const fact = res.data.text;
-    numFactSection.insertAdjacentHTML('beforeend', `<p>${ fact }</p>`);
+axios
+  .get('http://numbersapi.com/42?json')
+  .then(f => {
+    numFactSection.insertAdjacentHTML('beforeend', `<p>${ f.data.text }</p>`);
   })
   .catch(err => console.log('ERROR!', err));
 
 const numsFactsSection = document.querySelector('#nums-facts-section');
-let numsFacts = axios.get('http://numbersapi.com/7..9,24?json');
 
-numsFacts
-  .then(res => {
-    const facts = Object.values(res.data);
+axios
+  .get('http://numbersapi.com/7..9,24?json')
+  .then(f => {
+    const facts = Object.values(f.data);
     for(let i=0; i<facts.length; i++) {
       numsFactsSection.insertAdjacentHTML('beforeend', `<p>${ facts[i] }</p>`);
     }
   })
   .catch(err => console.log('ERROR!', err));
 
+const numFourFactsSection = document.querySelector('#num-four-facts-section');
+const fourNumFacts = [];
+
+for(let i=0; i<4; i++) {
+  fourNumFacts.push(
+    axios.get('http://numbersapi.com/15?json')
+  );
+}
+
+Promise
+  .all(fourNumFacts)
+  .then(factsArr => (
+    factsArr.forEach(f => numFourFactsSection.insertAdjacentHTML('beforeend', `<p>${ f.data.text }</p>`))
+  ))
+  .catch(err => console.log('ERROR!', err));
