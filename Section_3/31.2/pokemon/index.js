85dc4f86ac07efbbca969091c3c4c9e14215cd90
Nick Rudzis <nick.rudzis@gmail.com>
2023-02-06 11:45:10 -0800

    console.log pokemon name and species description

--- Section_3/31.2/pokemon/index.js
+++ Section_3/31.2/pokemon/index.js
@@ -14,7 +14,25 @@ axios
       }
     }
     for (const pokemon of randThreePokemon) {
-      console.log(pokemon);
+      const pName = pokemon.name;
+      axios
+        .get(pokemon.url)
+        .then(p => {
+          axios
+            .get(p.data.species.url)
+            .then(s => {
+              if (s.data.flavor_text_entries.length > 0) {
+                const entry = s.data.flavor_text_entries.find(e => e.language.name === 'en');
+                console.log('Name: ', pName );
+                console.log('Description: ', entry.flavor_text);
+              } else {
+                console.log('Name: ', pName);
+                console.log('Description: No description available.');
+              }
+            })
+            .catch(err => console.log('ERROR!', err));
+        })
+        .catch(err => console.log('ERROR!', err));
     }
   })
   .catch(err => console.log('ERROR!', err));
