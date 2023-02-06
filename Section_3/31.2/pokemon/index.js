3d8abf93ad28ceed83948c407d4199c7f2ddfa08
Nick Rudzis <nick.rudzis@gmail.com>
2023-02-06 14:32:56 -0800

    moved js logic into separate functions and added as methods on a Pokemon class

--- Section_3/31.2/pokemon/index.js
+++ Section_3/31.2/pokemon/index.js
@@ -1,38 +1,59 @@
-axios
-  .get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
-  .then(pAll => {
-    const pokemonAll = [];
-    pAll.data.results.forEach(p => {
-      pokemonAll.push({ 'name': p.name, 'url': p.url });
-    })
-    const randThreePokemon = [];
-    while (randThreePokemon.length < 3) {
-      let randIndex = Math.floor(Math.random() * pokemonAll.length);
-      let randPokemon = pokemonAll[randIndex];
-      if (!randThreePokemon.includes(randPokemon)) {
-        randThreePokemon.push(randPokemon);
-      }
-    }
-    for (const pokemon of randThreePokemon) {
-      const pName = pokemon.name;
-      axios
-        .get(pokemon.url)
-        .then(p => {
-          axios
-            .get(p.data.species.url)
-            .then(s => {
-              if (s.data.flavor_text_entries.length > 0) {
-                const entry = s.data.flavor_text_entries.find(e => e.language.name === 'en');
-                console.log('Name: ', pName );
-                console.log('Description: ', entry.flavor_text);
-              } else {
-                console.log('Name: ', pName);
-                console.log('Description: No description available.');
-              }
-            })
-            .catch(err => console.log('ERROR!', err));
-        })
-        .catch(err => console.log('ERROR!', err));
-    }
-  })
-  .catch(err => console.log('ERROR!', err));
+class Pokemon {
+
+  getThreeRandPokemon() {
+    return axios
+      .get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+      .then(pAll => {
+        const randThreePokemon = [];
+        while (randThreePokemon.length < 3) {
+          let randIndex = Math.floor(Math.random() * pAll.data.results.length);
+          let randPokemon = pAll.data.results[randIndex];
+          if (!randThreePokemon.includes(randPokemon)) {
+            randThreePokemon.push(randPokemon);
+          }
+        }
+        return randThreePokemon;
+      })
+      .catch(err => console.log('ERROR!', err));
+  }
+
+  getSpeciesURL(pokemon) {
+    return axios
+      .get(pokemon.url)
+      .then(p => {
+        return p.data.species.url
+      })
+      .catch(err => console.log('ERROR!', err));
+  }
+
+  getDescription(speciesURL) {
+    return axios
+      .get(speciesURL)
+      .then(s => {
+        if (s.data.flavor_text_entries.length > 0) {
+          const entry = s.data.flavor_text_entries.find(e => e.language.name === 'en');
+          return `Description: ${ entry.flavor_text }`;
+        } else {
+          return 'Description: No description available.';
+        }
+      })
+      .catch(err => console.log('ERROR!', err));
+  }
+
+  consoleLogThreePokemon() {
+    this.getThreeRandPokemon()
+      .then(threeRandPokemon => {
+        for (const pokemon of threeRandPokemon) {
+          const pName = pokemon.name;
+          this.getSpeciesURL(pokemon)
+            .then(speciesURL => {
+              this.getDescription(speciesURL)
+                .then(pDescription => {
+                  console.log({'name': pName, 'description': pDescription});
+                });
+            });
+        }
+      });
+  }
+
+}
