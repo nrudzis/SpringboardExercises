3e30774548d2339c8537f67a7d320905b94bbbcd
Nick Rudzis <nick.rudzis@gmail.com>
2023-02-06 17:50:00 -0800

    add js/html to display names and descriptions on page

--- Section_3/31.2/pokemon/index.js
+++ Section_3/31.2/pokemon/index.js
@@ -1,18 +1,15 @@
 class Pokemon {
 
-  getThreeRandPokemon() {
+  constructor() {
+    this.threeRandPokemon = this.getThreeRandPokemon()
+  }
+
+  getRandPokemon() {
     return axios
       .get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
       .then(pAll => {
-        const randThreePokemon = [];
-        while (randThreePokemon.length < 3) {
-          let randIndex = Math.floor(Math.random() * pAll.data.results.length);
-          let randPokemon = pAll.data.results[randIndex];
-          if (!randThreePokemon.includes(randPokemon)) {
-            randThreePokemon.push(randPokemon);
-          }
-        }
-        return randThreePokemon;
+        const randIndex = Math.floor(Math.random() * pAll.data.results.length);
+        return pAll.data.results[randIndex];
       })
       .catch(err => console.log('ERROR!', err));
   }
@@ -21,9 +18,17 @@ class Pokemon {
     return axios
       .get(pokemon.url)
       .then(p => {
-        return p.data.species.url
+        return p.data.species.url;
       })
       .catch(err => console.log('ERROR!', err));
+  }
+
+  getFormsURL(pokemon) {
+    return axios
+      .get(pokemon.url)
+      .then(p => {
+        return p.data.forms[0]['url']
+      })
   }
 
   getDescription(speciesURL) {
@@ -32,28 +37,61 @@ class Pokemon {
       .then(s => {
         if (s.data.flavor_text_entries.length > 0) {
           const entry = s.data.flavor_text_entries.find(e => e.language.name === 'en');
-          return `Description: ${ entry.flavor_text }`;
+          return `${ entry.flavor_text }`;
         } else {
-          return 'Description: No description available.';
+          return 'No description available.';
         }
       })
       .catch(err => console.log('ERROR!', err));
   }
 
-  consoleLogThreePokemon() {
-    this.getThreeRandPokemon()
-      .then(threeRandPokemon => {
-        for (const pokemon of threeRandPokemon) {
-          const pName = pokemon.name;
-          this.getSpeciesURL(pokemon)
-            .then(speciesURL => {
-              this.getDescription(speciesURL)
-                .then(pDescription => {
-                  console.log({'name': pName, 'description': pDescription});
-                });
-            });
-        }
+  getNameDescription() {
+    return this.getRandPokemon()
+      .then(randPokemon => {
+        const pName = randPokemon.name;
+        return this.getSpeciesURL(randPokemon)
+          .then(speciesURL => {
+            return this.getDescription(speciesURL)
+              .then(pDescription => {
+                return {'name': pName, 'description': pDescription};
+              });
+          });
       });
   }
 
+  getThreeRandPokemon() {
+    let threeRandPokemon = []
+    for (let i=0; i<3; i++) {
+      this.getNameDescription().then(p => {
+        threeRandPokemon.push(p);
+      });
+    }
+    return threeRandPokemon;
+  }
+
 }
+
+const pokemonSection = document.getElementById('pokemon-section');
+const pokemonBtn = document.getElementById('pokemon-btn');
+let pokemon = new Pokemon();
+
+const displayPokemon = (ps) => {
+  pokemonSection.innerHTML = '';
+  for (let p of ps) {
+    const div = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const par = document.createElement('p');
+    h2.innerHTML = p.name;
+    par.innerHTML = p.description;
+    div.appendChild(h2);
+    div.appendChild(par);
+    pokemonSection.appendChild(div);
+  }
+  pokemon = new Pokemon();
+}
+
+
+pokemonBtn.addEventListener('click', e => {
+  const threeRandPokemon = pokemon.threeRandPokemon;
+  displayPokemon(threeRandPokemon);
+});
