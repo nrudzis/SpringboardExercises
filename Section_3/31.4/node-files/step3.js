49e037ae8e2ab500957d78020e3ab14d8925925d
Nick Rudzis <nick.rudzis@gmail.com>
2023-02-11 16:56:54 -0800

    add ability to take any number of arguments

--- Section_3/31.4/node-files/step3.js
+++ Section_3/31.4/node-files/step3.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const axios = require('axios');
-const arg3 = process.argv[2];
+const args = process.argv;
+const isWriteOut = args[2] === '--out';
 
-const writeOut = (data) => {
-  const outFile = process.argv[3];
+const writeOut = (i, data) => {
+  const outFiles = args.slice(3).filter((_, index) => index % 2 == 0);
+  console.log(outFiles);
+  const outFile = outFiles[i];
   fs.writeFile(`./${ outFile }`, data, 'utf8', err => {
     if (err) {
       console.log('ERROR: ', err);
@@ -13,24 +16,24 @@ const writeOut = (data) => {
   });
 }
 
-const cat = (path) => {
+const cat = (i, path) => {
   fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
       console.log('ERROR: ', err);
       process.exit(1);
-    } else if (arg3 === '--out') {
-      writeOut(data);
+    } else if (isWriteOut) {
+      writeOut(i, data);
     } else {
       console.log(data);
     }
   });
 }
 
-const webCat = async function (url) {
+const webCat = async function (i, url) {
   try {
     const { data: content } = await axios.get(url);
-    if (arg3 === '--out') {
-      writeOut(content);
+    if (isWriteOut) {
+      writeOut(i, content);
     } else {
     console.log(content);
     }
@@ -39,9 +42,9 @@ const webCat = async function (url) {
   }
 }
 
-if (arg3 != '--out') {
-  arg3.startsWith('http') ? webCat(arg3) : cat(arg3);
-} else {
-  const arg5 = process.argv[4];
-  arg5.startsWith('http') ? webCat(arg5) : cat(arg5);
-}
+let inputSources;
+!isWriteOut ? inputSources = args.slice(2) : inputSources = args.slice(3).filter((_, index) => index % 2 != 0);
+console.log(inputSources);
+inputSources.forEach((inputSource, index) => {
+  inputSource.startsWith('http') ? webCat(index, inputSource) : cat(index, inputSource);
+});
