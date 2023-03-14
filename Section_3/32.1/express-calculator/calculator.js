899eb9719e9129ac9836728524d17957101a464d
Nick Rudzis <nick.rudzis@gmail.com>
2023-03-14 14:15:20 -0700

    add initial code for mean route

--- Section_3/32.1/express-calculator/calculator.js
+++ Section_3/32.1/express-calculator/calculator.js
@@ -2,6 +2,22 @@ const express = require('express');
 
 const app = express();
 
+const operations = {
+  mean: function(nums) {
+    const numsArr = nums.split(',').map(numStr => parseFloat(numStr));
+    return numsArr.reduce((total, current) => total + current) / numsArr.length;
+  },
+  median: 'MEDIAN',
+  mode: 'MODE'
+}
+
+app.get('/:operation', (req, res) => {
+  const operation = req.params.operation;
+  const { nums } = req.query;
+  const result = operations[operation](nums)
+  return res.send(result.toString());
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
