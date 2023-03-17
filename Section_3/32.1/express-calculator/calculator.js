608fe8330cd339cd0a6da0d35356992d6112296a
Nick Rudzis <nick.rudzis@gmail.com>
2023-03-17 11:48:34 -0700

    add median and mode calculations

--- Section_3/32.1/express-calculator/calculator.js
+++ Section_3/32.1/express-calculator/calculator.js
@@ -10,8 +10,34 @@ const OPERATIONS = {
     const mean = numsArr.reduce((total, current) => total + current) / numsArr.length;
     return mean;
   },
-  median: 'MEDIAN',
-  mode: 'MODE'
+  medianCalc: function(nums) {
+    let median;
+    const numsArr = nums.split(',').map(num => parseFloat(num)).sort((a, b) => a - b);
+    const numsArrLen = numsArr.length;
+    if (numsArrLen % 2 === 0) {
+      median = (numsArr[numsArrLen / 2 - 1] + numsArr[numsArrLen / 2]) / 2;
+    } else {
+      median = numsArr[Math.floor(numsArrLen / 2)];
+    }
+    return median;
+  },
+  modeCalc: function(nums) {
+    const numsArr = nums.split(',').map(num => parseFloat(num)).sort((a, b) => a - b);
+    const numsArrCounts = {};
+    numsArr.forEach(num => !numsArrCounts[num] ? numsArrCounts[num] = 1 : numsArrCounts[num]++);
+    let mode;
+    const numsArrCountsVals = Object.values(numsArrCounts);
+    const numsArrCountsKeys = Object.keys(numsArrCounts);
+    const maxVal = Math.max(...numsArrCountsVals);
+    if (numsArr.length === numsArrCountsVals.length) {
+      mode = null;
+    } else if (numsArrCountsVals.filter(val => val === maxVal).length === 1) {
+      mode = parseFloat(numsArrCountsKeys.find(key => numsArrCounts[key] === maxVal));
+    } else {
+      mode = numsArrCountsKeys.filter(key => numsArrCounts[key] === maxVal).map(num => parseFloat(num));
+    }
+    return mode;
+  }
 }
 
 app.get('/:operation', (req, res) => {
