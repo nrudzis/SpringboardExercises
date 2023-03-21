af8bb59bc155fb9889c797f0dfdc98895edd8b5b
Nick Rudzis <nick.rudzis@gmail.com>
2023-03-21 04:11:24 -0700

    create ExpressError class, add error handling to app

--- Section_3/32.1/express-calculator/calculator.js
+++ Section_3/32.1/express-calculator/calculator.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ExpressError = require('./expressError');
 
 const app = express();
 
@@ -40,11 +41,30 @@ const OPERATIONS = {
   }
 }
 
-app.get('/:operation', (req, res) => {
-  const operation = req.params.operation;
-  const { nums } = req.query;
-  const result = OPERATIONS[`${operation}Calc`](nums);
-  return res.json({ 'operation': operation, 'value': result });
+app.get('/:operation', (req, res, next) => {
+  try {
+    const operation = req.params.operation;
+    if (['mean', 'median', 'mode'].indexOf(operation) < 0) throw new ExpressError('Not Found', 404);
+    const { nums } = req.query;
+    if (!nums || nums.split(',').length < 2 || !nums.split(',').every(elem => !isNaN(elem))) throw new ExpressError('Bad Request', 400);
+    const result = OPERATIONS[`${operation}Calc`](nums);
+    return res.json({ 'operation': operation, 'value': result });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.use((req, res, next) => {
+  const notFoundError = new ExpressError('Not Found', 404);
+  return next(notFoundError);
+});
+
+app.use((err, req, res, next) => {
+  let status = err.status || 500;
+  let msg = err.msg;
+  return res.status(status).json({
+    error: {msg, status}
+  });
 });
 
 app.listen(3000, () => {
