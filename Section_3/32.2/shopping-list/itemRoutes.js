0d4c067e4593902422b9390a4c5e76db6f3bbdbc
Nick Rudzis <nick.rudzis@gmail.com>
2023-03-22 15:58:12 -0700

    add patch route

--- Section_3/32.2/shopping-list/itemRoutes.js
+++ Section_3/32.2/shopping-list/itemRoutes.js
@@ -7,16 +7,22 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  const name = req.body.name;
-  const price = req.body.price;
-  ITEMS.push({ 'name': name, 'price': price });
-  const item = ITEMS.find(i => i.name === name)
+  ITEMS.push({ 'name': req.body.name, 'price': req.body.price });
+  const item = ITEMS.find(i => i.name === req.body.name)
   return res.json({ 'added': item });
 });
 
 router.get('/:name', (req, res) => {
-  const item = ITEMS.find(i => i.name === req.params.name)
+  const item = ITEMS.find(i => i.name === req.params.name);
   return res.json({ item });
+});
+
+router.patch('/:name', (req, res) => {
+  const item = ITEMS.find(i => i.name === req.params.name);
+  item.name = req.body.name;
+  item.price = req.body.price;
+  const itemUpdated = ITEMS.find(i => i.name === req.body.name)
+  return res.json({ 'updated': itemUpdated });
 });
 
 module.exports = router;
