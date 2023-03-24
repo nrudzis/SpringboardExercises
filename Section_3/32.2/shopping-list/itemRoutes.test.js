8796177fdd44d70b04ef19a453233356cbb6bc2c
Nick Rudzis <nick.rudzis@gmail.com>
2023-03-24 15:12:42 -0700

    add test to get valid and invalid individual name

--- Section_3/32.2/shopping-list/itemRoutes.test.js
+++ Section_3/32.2/shopping-list/itemRoutes.test.js
@@ -37,6 +37,21 @@ describe('POST /items', () => {
   });
 });
 
+describe('GET /items/:name', () => {
+  test('should get an item from the list', async () => {
+    const res = await req(app).get(`/items/${chocolate.name}`);
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ item: chocolate });
+  });
+  test('should respond with 404 with invalid name to get', async () => {
+    const res = await req(app).get('/items/wrong_name');
+    expect(res.statusCode).toBe(404);
+    expect(res.body).toEqual({
+      error: { msg: 'Unable to find item', status: 404 }
+    });
+  });
+});
+
 describe('PATCH /items/:name', () => {
   test('should update an item in the list', async () => {
     const res = await req(app)
@@ -50,7 +65,7 @@ describe('PATCH /items/:name', () => {
       updated: { name: 'white_chocolate', price: 9.89 }
     });
   });
-  test('should respond with 404 for invalid name', async () => {
+  test('should respond with 404 for invalid name to update', async () => {
     const res = await req(app)
       .patch('/items/wrong_name')
       .send({
@@ -69,5 +84,12 @@ describe('DELETE /items/:name', () => {
     const res = await req(app).delete(`/items/${chocolate.name}`);
     expect(res.statusCode).toBe(200);
     expect(res.body).toEqual({ message: 'Deleted' });
+  });
+  test('should respond with 404 for invalid name to delete', async () => {
+    const res = await req(app).delete('/items/wrong_name');
+    expect(res.statusCode).toBe(404);
+    expect(res.body).toEqual({
+      error: { msg: 'Unable to find item to delete', status: 404 }
+    });
   });
 });
