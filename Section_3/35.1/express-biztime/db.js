6d742fa7d9d9ac0ff2473bf2ee0c7a4f003b9e97
Nick Rudzis <nick.rudzis@gmail.com>
2023-05-11 23:20:46 -0700

    add logic to db file to connect to biztime database and export the Client object

--- Section_3/35.1/express-biztime/db.js
+++ Section_3/35.1/express-biztime/db.js
@@ -1,2 +1,13 @@
 /** Database setup for BizTime. */
 
+const { Client } = require('pg');
+
+const DB_URI = 'postgresql:///biztime';
+
+const db = new Client({
+  connectionString: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
