1c5c436e7cc9b1d2e2479f1243526fff52292c86
Nick Rudzis <nick.rudzis@gmail.com>
2023-08-03 17:15:35 -0700

    add initial register and authenticate method logic to User class

--- Section_3/36.1/express-messagely/models/user.js
+++ Section_3/36.1/express-messagely/models/user.js
@@ -1,6 +1,9 @@
 /** User class for message.ly */
 
-
+const bcrypt = require("bcrypt");
+const db = require("db");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+const ExpressError = require("../expressError");
 
 /** User of the site. */
 
@@ -10,11 +13,38 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({username, password, first_name, last_name, phone}) {
+    try {
+      const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+      const joinAt = new Date();
+      const result = await db.query(
+      `INSERT INTO users (username, password, first_name, last_name, phone, join_at)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone, joinAt]);
+      return res.json(result.rows[0]);
+    } catch (err) {
+      return next(err);
+    }
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    try {
+      const result = await db.query(
+      `SELECT password FROM users WHERE username = $1`,
+      [username]);
+      const user = result.rows[0];
+      if (user) {
+        const isValid = await bcrypt.compare(password, user.password);
+        return isValid;
+      }
+      throw new ExpressError("Invalid username or password", 400);
+    } catch (err) {
+      return next(err);
+    }
+  }
 
   /** Update last_login_at for user */
 
