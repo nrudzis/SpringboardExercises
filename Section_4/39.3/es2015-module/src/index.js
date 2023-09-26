769ef407f946f74da4a1200eabefbaf7bcfd85e7
Nick Rudzis <nick.rudzis@gmail.com>
2023-09-26 06:05:13 -0700

    add imports, index logic

--- Section_4/39.3/es2015-module/src/index.js
+++ Section_4/39.3/es2015-module/src/index.js
@@ -1,3 +1,14 @@
-const fruit = ["grapes", "melon", "watermelon", "orange", "lemon", "banana", "pineapple", "red apple", "green apple", "pear", "cherries", "strawberry", "kiwi", "tomato", "avocado"];
+import foods from './foods';
+import {choice, remove} from './helpers';
 
-export default fruit;
+let randFruit = choice(foods);
+
+console.log(`I'd like one ${randFruit}, please.`);
+
+let removedFruit = remove(foods, randFruit);
+
+console.log(`Here you go: ${removedFruit}`);
+
+console.log("Delicious! May I have another?");
+
+console.log(`I'm sorry, we're all out. We have ${foods.length} left.`);
