d36cb6d3a4c8b57806740ed1e2e28d8cf124182f
Nick Rudzis <nick.rudzis@gmail.com>
2023-09-30 02:37:16 -0700

    remove unneeded /src files, add default pokemon, modify App component to display basic info

--- Section_4/39.3/pokedex/src/App.js
+++ Section_4/39.3/pokedex/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import defaultPokemon from './defaultPokemon';
 
-function App() {
+const App = ({pokemon = defaultPokemon}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {pokemon.map(p => (
+        <div>
+          <h4>{p.name}</h4>
+          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`} alt='Pokemon character'/>
+          <p>Type: {p.type}</p>
+          <p>EXP: {p.base_experience}</p>
+        </div>
+      ))}
     </div>
   );
 }
