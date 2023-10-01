ae41d399dc0089dd29910efa77881e4e262c2102
Nick Rudzis <nick.rudzis@gmail.com>
2023-10-01 01:25:05 -0700

    add styles to make div a circle and center message

--- Section_4/39.4/eight-ball/src/EightBall.js
+++ Section_4/39.4/eight-ball/src/EightBall.js
@@ -1,15 +1,18 @@
 import { useState } from 'react';
+import './EightBall.css';
 
 const randIndex = (arr) => {
   return Math.floor(Math.random() * arr.length);
 }
 
 const EightBall = ({ answers }) => {
-  const [message, setMessage] = useState("Think of a Question");
+  const [message, setMessage] = useState('Think of a Question');
   const randAnswer = answers[randIndex(answers)].msg;
   return (
-    <div onClick={() => setMessage(randAnswer)}>
-      { message }
+    <div className='EightBall'>
+      <div className='EightBall-msg' onClick={() => setMessage(randAnswer)}>
+        { message }
+      </div>
     </div>
   );
 }
