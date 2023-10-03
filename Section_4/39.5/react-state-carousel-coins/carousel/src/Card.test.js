9752b256bee2af6c5d9eefc6771be9b3430c2a53
Nick Rudzis <nick.rudzis@gmail.com>
2023-10-03 08:12:57 -0700

    add snapshot tests for Card and Carousel components

--- Section_4/39.5/react-state-carousel-coins/carousel/src/Card.test.js
+++ Section_4/39.5/react-state-carousel-coins/carousel/src/Card.test.js
@@ -5,3 +5,8 @@ import Card from "./Card";
 it("renders without crashing", function() {
   render(<Card />);
 });
+
+it("matches snapshot", function() {
+  const { asFragment } = render(<Card />);
+  expect(asFragment()).toMatchSnapshot();
+});
