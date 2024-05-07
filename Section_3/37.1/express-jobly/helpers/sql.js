46dfd60c9a8b37885dc7fdb3dda5ce5be30b8ef4
Nick Rudzis <nick.rudzis@gmail.com>
2024-05-06 21:53:04 -0700

    add documentation for sqlForPartialUpdate

--- Section_3/37.1/express-jobly/helpers/sql.js
+++ Section_3/37.1/express-jobly/helpers/sql.js
@@ -1,6 +1,26 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Generic helper function for partial updates. Used by `update` methods on
+ * models.
+ *
+ * Takes update data and object mapping particular model JS-formatted keys to
+ * JSON-formatted keys (camelCased to snake_cased).
+ *
+ * Returns parameterized SQL string of columns to set and array of values to update to.
+ *
+ * Throws BadRequestError if no keys found in data.
+ *
+ * Company example:
+ *   ({ name: "New Company Name", numEmployees: 18 },
+ *   { numEmployees: "num_employees", logoUrl: "logo_url" }) => 
+ *   { setCols: '"name"=$1, "num_employees"=$2', values: ["New Company Name", 18] }
+ *
+ * User example:
+ *   ({ firstName: "Newname", email: "newemail@blabla.com", isAdmin: true },
+ *   { firstName: "first_name", lastName: "last_name", isAdmin: "is_admin"}) =>
+ *   { setCols: '"first_name"=$1, "email"=$2, "is_admin"=$3',
+ *   values: ["Newname", "newemail@blabla.com", true] }
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
