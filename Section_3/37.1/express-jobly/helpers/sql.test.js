9543f627bbe35f1d365fe57a1c49b34cb29b9937
Nick Rudzis <nick.rudzis@gmail.com>
2024-05-21 09:53:07 -0700

    add unit tests for filter companies helper func, improve test file organization

--- Section_3/37.1/express-jobly/helpers/sql.test.js
+++ Section_3/37.1/express-jobly/helpers/sql.test.js
@@ -1,7 +1,10 @@
-const { sqlForPartialUpdate } = require("./sql");
+const { sqlForPartialUpdate, sqlForFilterCompanies } = require("./sql");
 const { BadRequestError } = require("../expressError");
 
-// Initial data and expected results 
+
+/************************************** sqlForPartialUpdate
+ * initial data and expected results
+ */
 
 const errorData = {};
 const companyData = {
@@ -31,7 +34,9 @@ const userCorrectResult = {
   values: ["Testname", "testemail@abcd.com", true]
 };
 
-//Tests
+/************************************** sqlForPartialUpdate
+ * tests
+ */
 
 describe("sqlForPartialUpdate", function () {
 
@@ -53,5 +58,54 @@ describe("sqlForPartialUpdate", function () {
   test("outputs expected result with user data", function () {
     const userResult = sqlForPartialUpdate(userData, userSqlToJs);
     expect(userResult).toEqual(userCorrectResult);
+  });
+});
+
+/************************************** sqlForFilterCompanies
+ * params and expected results
+ */
+
+const invalidParams = {
+  invalid: "iminvalid"
+};
+const badMinMaxVals = {
+  minEmployees: 31,
+  maxEmployees: 30
+};
+const correctParams = {
+  name: 'net',
+  minEmployees: 65,
+  maxEmployees: 500
+};
+const correctResult = {
+  whereCols: '"name" ILIKE $1 AND "num_employees">=$2 AND "num_employees"<=$3',
+  values: ["%net%", 65, 500]
+};
+
+/************************************** sqlForFilterCompanies
+ * tests
+ */
+
+describe("sqlForFilterCompanies", function() {
+
+  test("is a function", function () {
+    expect(typeof sqlForFilterCompanies).toEqual("function");
+  });
+
+  test("throws BadRequestError if params are invalid", function () {
+    expect(function () {
+      sqlForFilterCompanies(invalidParams);
+    }).toThrow(BadRequestError);
+  });
+
+  test("throws BadRequestError if maxEmployees is less than minEmployees", function () {
+    expect(function () {
+      sqlForFilterCompanies(badMinMaxVals);
+    }).toThrow(BadRequestError);
+  });
+
+  test("outputs expected result with correct params", function() {
+    const result = sqlForFilterCompanies(correctParams);
+    expect(result).toEqual(correctResult);
   });
 });
