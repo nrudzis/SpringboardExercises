40c40617d7fd2324487eb5e609283c9e6ad94495
Nick Rudzis <nick.rudzis@gmail.com>
2024-06-23 15:27:46 -0700

    add countZeroes logic

--- Section_5/47.3/prob-divide-conquer/count-zeroes.js
+++ Section_5/47.3/prob-divide-conquer/count-zeroes.js
@@ -1,5 +1,17 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+  let zeroCount = 0;
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === 1) {
+      left = mid + 1;
+    } else {
+      zeroCount += right - mid + 1;
+      right = mid - 1;
+    }
+  }
+  return zeroCount;
 }
 
 module.exports = countZeroes
