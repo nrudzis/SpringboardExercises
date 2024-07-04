47360c6a4c11e6a4bec7512c01f5c829bd2a150c
Nick Rudzis <nick.rudzis@gmail.com>
2024-07-03 18:53:59 -0700

    reduce lines of code and variables in sortedFrequency

--- Section_5/47.3/prob-divide-conquer/sorted-frequency.js
+++ Section_5/47.3/prob-divide-conquer/sorted-frequency.js
@@ -1,56 +1,37 @@
 function sortedFrequency(arr, num) {
   let left = 0;
   let right = arr.length - 1;
-  let firstInstance = -1;
-  let lastInstance = -1;
+
+  // binary search for first instance of num
   while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] === num && (arr[mid - 1] < num || mid === 0)) {
+      let firstInst = mid;
+      left = mid;
+      right = arr.length - 1;
 
-    // if mid is the index of num, perform binary search on each side
-    if (arr[mid] === num) {
-      firstInstance = mid;
-      lastInstance = mid;
-      let firstArrLeft = left;
-      let firstArrRight = mid - 1;
-      let secondArrLeft = mid + 1;
-      let secondArrRight = right;
-
-      // binary search for firstInstance
-      while(firstArrLeft <= firstArrRight) {
-        let firstArrMid = Math.floor((firstArrLeft + firstArrRight) / 2);
-        if (arr[firstArrMid] === num) {
-          firstInstance = firstArrMid;
-          firstArrRight = firstArrMid - 1;
+      // binary search for last instance of num
+      while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+        if (arr[mid] === num && (arr[mid + 1] > num || mid === arr.length - 1)) {
+          let lastInst = mid;
+          return lastInst - firstInst + 1;
+        } else if (arr[mid] > num) {
+          right = mid - 1;
         } else {
-          firstArrLeft = firstArrMid + 1;
+          left = mid;
         }
       }
-
-      // binary search for lastInstance
-      while(secondArrLeft <= secondArrRight) {
-        let secondArrMid = Math.floor((secondArrLeft + secondArrRight) / 2);
-        if (arr[secondArrMid] === num) {
-          lastInstance = secondArrMid;
-          secondArrLeft = secondArrMid + 1;
-        } else {
-          secondArrRight = secondArrMid - 1;
-        }
-      }
-      break;
-    }
-
     // if mid is the index of a number < num, search to the right
-    else if (arr[mid] < num) {
+    } else if (arr[mid] < num) {
       left = mid + 1;
 
     // otherwise, search to the left
     } else {
-      right = mid - 1;
+      right = mid;
     }
-  } if (firstInstance === -1 || lastInstance === -1) {
-    return -1;
   }
-  return lastInstance - firstInstance + 1;
+  return -1;
 }
 
 module.exports = sortedFrequency;
