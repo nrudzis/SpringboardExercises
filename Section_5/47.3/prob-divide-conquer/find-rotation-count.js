69dfecd088af1ef8855c749b3f55d076dca88c01
Nick Rudzis <nick.rudzis@gmail.com>
2024-07-03 18:18:01 -0700

    add logic for findRotationCount

--- Section_5/47.3/prob-divide-conquer/find-rotation-count.js
+++ Section_5/47.3/prob-divide-conquer/find-rotation-count.js
@@ -1,5 +1,23 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  // binary search for index of first element of unrotated array
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] < arr[mid - 1]) {
+      return mid;
+
+    // if mid is the index of a number > than the number at index right, search to the right
+    } else if (arr[mid] > arr[right]) {
+      left = mid + 1;
+
+    // otherwise, search to the left
+    } else {
+      right = mid - 1;
+    }
+  }
+  return 0;
 }
 
 module.exports = findRotationCount
