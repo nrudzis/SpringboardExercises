a6344db27685cebf6d88946fc804c76a55edd4ad
Nick Rudzis <nick.rudzis@gmail.com>
2024-07-03 18:27:09 -0700

    add logic for findFloor

--- Section_5/47.3/prob-divide-conquer/find-floor.js
+++ Section_5/47.3/prob-divide-conquer/find-floor.js
@@ -1,5 +1,18 @@
-function findFloor() {
-  
+function findFloor(arr, x) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] <= x && (arr[mid + 1] > x || arr[mid + 1] === undefined)) {
+      return arr[mid];
+    } else if (arr[mid] < x) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
 }
 
 module.exports = findFloor
