bd671efcb6aca943ff8500d6c6a214bf20e35956
Nick Rudzis <nick.rudzis@gmail.com>
2024-11-21 09:53:42 -0800

    add logic for Tree class methods

--- Section_5/47.8/dsa-trees/tree.js
+++ Section_5/47.8/dsa-trees/tree.js
@@ -15,20 +15,54 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    let queue = [this.root];
+    if (queue[0] === null) return 0;
+    let sum = 0;
+    while (queue.length) {
+      let current = queue.pop();
+      sum = sum + current.val;
+      for (let child of current.children) {
+        queue.push(child);
+      }
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
-
+    let queue = [this.root];
+    if (queue[0] === null) return 0;
+    let evenCount = 0;
+    while (queue.length) {
+      let current = queue.pop();
+      if (current.val % 2 === 0) {
+        evenCount++;
+      }
+      for (let child of current.children) {
+        queue.push(child);
+      }
+    }
+    return evenCount;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
-
+    let queue = [this.root];
+    if (queue[0] === null) return 0;
+    let countGreater = 0;
+    while (queue.length) {
+      let current = queue.pop();
+      if (current.val > lowerBound) {
+        countGreater++;
+      }
+      for (let child of current.children) {
+        queue.push(child);
+      }
+    }
+    return countGreater;
   }
 }
 
