17a904ba9c9102f3cb3d51ce2d3d475c1a818725
Nick Rudzis <nick.rudzis@gmail.com>
2025-09-01 09:53:00 -0700

    add logic for push, pop, peek, and isEmpty methods in Stack class

--- Section_5/47.5/dsa-stacks-queues/stack.js
+++ Section_5/47.5/dsa-stacks-queues/stack.js
@@ -17,29 +17,51 @@ class Stack {
     this.size = 0;
   }
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+  /** push(val): add new value to top of the stack. Returns undefined. */
 
   push(val) {
-
+    let newNode = new Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    this.size++;
+    return undefined;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if (this.size === 0) throw new Error("Stack is empty! Cannot pop.")
+    let poppedVal = this.first.val;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+    this.size--;
+    return poppedVal;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    if (this.size === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
