492d18e837a4f2c2b61412a1d5ef55185fd5ace1
Nick Rudzis <nick.rudzis@gmail.com>
2025-09-02 18:21:51 -0700

    add logic for addVertex, addVertices, and addEdge methods in Graph class

--- Section_5/47.10/dsa-graphs/graph.js
+++ Section_5/47.10/dsa-graphs/graph.js
@@ -11,25 +11,34 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.nodes.add(vertex);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
-
-  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
-
-  // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
-
-  // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+//  removeEdge(v1, v2) {}
+//
+//  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+//  removeVertex(vertex) {}
+//
+//  // this function returns an array of Node values using DFS
+//  depthFirstSearch(start) {}
+//
+//  // this function returns an array of Node values using BFS
+//  breadthFirstSearch(start) {}
 }
 
 module.exports = {Graph, Node}
