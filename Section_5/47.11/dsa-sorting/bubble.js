8ba27698c9a7897bda18af4e61872df5f504cb40
Nick Rudzis <nick.rudzis@gmail.com>
2025-09-04 19:51:20 -0700

    implement bubbleSort function

--- Section_5/47.11/dsa-sorting/bubble.js
+++ Section_5/47.11/dsa-sorting/bubble.js
@@ -1,3 +1,14 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+  for (let i = arr.length-1; i > -1; i--) {
+    for (let j = 0; j <= i-1; j++) {
+      if (arr[j] > arr[j+1]) {
+        let bubble = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = bubble;
+      }
+    }
+  }
+  return arr;
+};
 
 module.exports = bubbleSort;
