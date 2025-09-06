f5f0623dfa3e2e8f1ca2b329246b88875fec5e1d
Nick Rudzis <nick.rudzis@gmail.com>
2025-09-06 15:36:00 -0700

    implement merge function

--- Section_5/47.11/dsa-sorting/merge.js
+++ Section_5/47.11/dsa-sorting/merge.js
@@ -1,5 +1,30 @@
-function merge() {}
+function merge(arr1, arr2) {
+  let partArr1 = [...arr1].reverse();
+  let partArr2 = [...arr2].reverse();
+  let mergedArr = [];
+  while (mergedArr.length < (arr1.length + arr2.length)) {
+    if (partArr1.length === 0) {
+      partArr2 = partArr2.reverse();
+      return [...mergedArr, ...partArr2];
+    } else if (partArr2.length === 0) {
+      partArr1 = partArr1.reverse();
+      return [...mergedArr, ...partArr1];
+    } else {
+      let num1 = partArr1[partArr1.length-1];
+      let num2 = partArr2[partArr2.length-1];
+      if (num1 < num2) {
+        mergedArr.push(num1);
+        partArr1.pop();
+      } else {
+        mergedArr.push(num2);
+        partArr2.pop();
+      }
+    }
+  }
+  return mergedArr;
+}
 
-function mergeSort() {}
+//function mergeSort() {}
 
-module.exports = { merge, mergeSort};
+module.exports = { merge };
+//module.exports = { merge, mergeSort };
