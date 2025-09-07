552909fd6cdc73ecbad4d681341bd90fe8594006
Nick Rudzis <nick.rudzis@gmail.com>
2025-09-06 18:29:59 -0700

    simplify logic of merge function

--- Section_5/47.11/dsa-sorting/merge.js
+++ Section_5/47.11/dsa-sorting/merge.js
@@ -1,27 +1,17 @@
 function merge(arr1, arr2) {
-  let partArr1 = [...arr1].reverse();
-  let partArr2 = [...arr2].reverse();
   let mergedArr = [];
-  while (mergedArr.length < (arr1.length + arr2.length)) {
-    if (partArr1.length === 0) {
-      partArr2 = partArr2.reverse();
-      return [...mergedArr, ...partArr2];
-    } else if (partArr2.length === 0) {
-      partArr1 = partArr1.reverse();
-      return [...mergedArr, ...partArr1];
+  let i = 0;
+  let j = 0;
+  while (arr1.length > i && arr2.length > j) {
+    if (arr1[i] < arr2[j]) {
+      mergedArr.push(arr1[i]);
+      i++;
     } else {
-      let num1 = partArr1[partArr1.length-1];
-      let num2 = partArr2[partArr2.length-1];
-      if (num1 < num2) {
-        mergedArr.push(num1);
-        partArr1.pop();
-      } else {
-        mergedArr.push(num2);
-        partArr2.pop();
-      }
+      mergedArr.push(arr2[j]);
+      j++
     }
   }
-  return mergedArr;
+  return mergedArr.concat(arr1.slice(i), arr2.slice(j));
 }
 
 function mergeSort(arr) {
